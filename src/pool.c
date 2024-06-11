65d29307a3dd0a873553d97262edd5dcd45093db
yanrbts <772166784@qq.com>
2024-06-11 18:34:51 +0800

    add webdis

--- src/pool.c
+++ src/pool.c
@@ -171,3 +171,85 @@ static void pool_on_auth_complete(redisAsyncContext *c, void *r, void *data) {
 	s->auth_logged++;
 	pthread_mutex_unlock(&s->auth_log_mutex);
 }
+
+/**
+ * Create new connection.
+ */
+redisAsyncContext *pool_connect(struct pool *p, int db_num, int attach) {
+    struct redisAsyncContext *ac;
+
+    if (p->cfg->redis_host[0] == '/') {
+        /* unix socket */
+        ac = redisAsyncConnectUnix(p->cfg->redis_host);
+    } else {
+        ac = redisAsyncConnect(p->cfg->redis_host, p->cfg->redis_port);
+    }
+
+    if(attach) {
+		ac->data = p;
+	} else {
+		ac->data = NULL;
+	}
+
+    if (ac->err) {
+        char msg[] = "Connection failed: %s";
+		size_t errlen = strlen(ac->errstr);
+		char *err = malloc(sizeof(msg) + errlen);
+		if (err) {
+			size_t sz = sprintf(err, msg, ac->errstr);
+			slog(p->w->s, WEBDIS_ERROR, err, sz);
+			free(err);
+		}
+		redisAsyncFree(ac);
+		pool_schedule_reconnect(p);
+		return NULL;
+    }
+
+#ifdef HAVE_SSL
+/* Negotiate SSL/TLS */
+	if(p->w->s->cfg->ssl.enabled) {
+		if(redisInitiateSSLWithContext((redisContext*)&ac->c, p->w->s->ssl_context) != REDIS_OK) {
+			/* Handle error, in c->err / c->errstr */
+			slog(p->w->s, WEBDIS_ERROR, "SSL negotiation failed", 0);
+			if(ac->c.err) { /* non-zero on error */
+				slog(p->w->s, WEBDIS_ERROR, ac->c.errstr, 0);
+			}
+			pool_schedule_reconnect(p);
+			return NULL;
+		}
+	}
+#endif
+
+	redisLibeventAttach(ac, p->w->base);
+	redisAsyncSetConnectCallback(ac, pool_on_connect);
+	redisAsyncSetDisconnectCallback(ac, pool_on_disconnect);
+
+    if(p->cfg->redis_auth) { /* authenticate. */
+		if(p->cfg->redis_auth->use_legacy_auth) {
+			redisAsyncCommand(ac, pool_on_auth_complete, p, "AUTH %s",
+				p->cfg->redis_auth->password);
+		} else {
+			redisAsyncCommand(ac, pool_on_auth_complete, p, "AUTH %s %s",
+				p->cfg->redis_auth->username,
+				p->cfg->redis_auth->password);
+		}
+	}
+	if(db_num) { /* change database. */
+		redisAsyncCommand(ac, NULL, NULL, "SELECT %d", db_num);
+	}
+	return ac;
+}
+
+const redisAsyncContext *pool_get_context(struct pool *p) {
+	int orig = p->cur++;
+
+	do {
+		p->cur++;
+		p->cur %= p->count;
+		if(p->ac[p->cur] != NULL) {
+			return p->ac[p->cur];
+		}
+	} while(p->cur != orig);
+
+	return NULL;
+}
